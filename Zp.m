bd06e15e370be4db7a08e9c7320b1948f742cf60
Mainak Das <98076918+Mainak27@users.noreply.github.com>
2024-09-24 23:16:44 +0530

    Update Zp.m

--- Zp.m
+++ Zp.m
@@ -1,14 +1,14 @@
 function Z=Zp(varargin)
 
-n=nargin; %numero de argumentos
-a=zeros(1,n); %se crea un vector de dimension n
-Z1=0; %defino la variable Z
+n=nargin; 
+a=zeros(1,n); 
+Z1=0; 
 
 for i=1:n
-    a(i) = varargin{i}; %cargo el vector
+    a(i) = varargin{i}; 
     Z1=(a(i))^-1 + Z1;
 end
 
-Z1=Z1^-1; %devuelvo el paralelo
+Z1=Z1^-1;
 
 Z=Z1;
